02e43bc7e34c001ef14564ef0ef72ea223a60311
Alby08 <aidenej@hotmail.com>
2024-06-03 03:51:22 +0800

    Feat: Created outline for index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
       "./*.html",
     "./assets/**/*.js",
   ],
   theme: {
+    colors: {
+      gray_950_alpha: '#030712d9',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
     extend: {},
   },
   plugins: [],
