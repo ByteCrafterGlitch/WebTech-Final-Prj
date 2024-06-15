89c03ed8c3db372dca5decf65bea00f6a5f97950
ByteCrafterGlitch <172339692+ByteCrafterGlitch@users.noreply.github.com>
2024-06-15 18:48:10 +0800

    Refactor: Update index code to be semantic

--- assets/js/hamburger-menu.js
+++ assets/js/hamburger-menu.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.getElementById("hamburger");
     const menu = document.getElementById("menu");
     const myName = document.getElementById("myName")
-    const mobileDiv = document.getElementById("mobileDiv")
+    const mobileNav = document.getElementById("mobileNav")
 
     hamburger.addEventListener("click", function() {
         menu.classList.toggle("hidden");
         menu.classList.toggle("flex");
         myName.classList.toggle("hidden");
         myName.classList.toggle("invisible");
-        mobileDiv.classList.toggle("hidden");
-        mobileDiv.classList.toggle("invisible");
-        mobileDiv.classList.toggle("flex");
+        mobileNav.classList.toggle("hidden");
+        mobileNav.classList.toggle("invisible");
+        mobileNav.classList.toggle("flex");
     });
 });
